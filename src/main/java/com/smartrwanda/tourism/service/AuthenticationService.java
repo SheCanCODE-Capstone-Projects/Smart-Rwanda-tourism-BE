bd6c34108329b5558a4a83631cedef05f387f46a
ILindah <irizalindah46@gmail.com>
2026-07-16 11:11:22 +0200

    Add googleLogin, logout, and getCurrentUser methods

--- src/main/java/com/smartrwanda/tourism/service/AuthenticationService.java
+++ src/main/java/com/smartrwanda/tourism/service/AuthenticationService.java
@@ -3,8 +3,18 @@ package com.smartrwanda.tourism.service;
 import com.smartrwanda.tourism.dto.*;
 
 public interface AuthenticationService {
+
     AuthResponse register(RegisterRequest request);
+
     AuthResponse login(LoginRequest request);
+
+    AuthResponse googleLogin(GoogleLoginRequest request);
+
     void forgotPassword(ForgotPasswordRequest request);
+
     void resetPassword(ResetPasswordRequest request);
+
+    void logout(String token);
+
+    UserResponse getCurrentUser();
 }
