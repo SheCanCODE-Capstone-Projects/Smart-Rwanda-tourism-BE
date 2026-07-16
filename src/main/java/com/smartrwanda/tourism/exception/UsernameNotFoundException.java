c1ee4e2f97fd3e7c98b200df38f1f3cb63421c77
uweradivi <uweradivine707@gmail.com>
2026-07-16 12:21:13 +0200

    feat: add UsernameNotFoundException

---
+++ src/main/java/com/smartrwanda/tourism/exception/UsernameNotFoundException.java
@@ -0,0 +1,10 @@
+package com.smartrwanda.tourism.exception;
+public class UsernameNotFoundException extends RuntimeException {
+    public UsernameNotFoundException(String message) {
+        super(message);
+    }
+
+    public UsernameNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
